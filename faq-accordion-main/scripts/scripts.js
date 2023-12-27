29f336ba6dad0793d7179e7b806f70c11358bd90
David Johansson <david.johansson4@tre.se>
2023-12-27 13:20:36 +0100

    Challenge 2: Fix accessibility for clickable links

--- faq-accordion-main/scripts/scripts.js
+++ faq-accordion-main/scripts/scripts.js
@@ -1,19 +1,22 @@
 const listener = (e) => {
-    const idOfClicked = e.target.id
-    const idOfHidden = `${idOfClicked}Over`;
-  //
-  // Get the reference to the element you want to hide
-   var elementToHide = document.getElementById(idOfHidden);
+  const idOfClicked = e.target.id
+  const identifier = idOfClicked.substr(0, idOfClicked.indexOf('_'));
+  const idOfHidden = `${identifier}Over`;
+  const image = document.getElementById(`${identifier}_img`)
 
-       if (elementToHide.style.maxHeight){
-      elementToHide.style.maxHeight = null;
-      e.target.src = "./assets/images/icon-plus.svg" 
-    } else {
-      elementToHide.style.maxHeight = elementToHide.scrollHeight + "px";
-      e.target.src = "./assets/images/icon-minus.svg" 
-    } }
+  // Get the reference to the element you want to hide
+  const elementToHide = document.getElementById(idOfHidden);
+
+  if (elementToHide.style.maxHeight) {
+    elementToHide.style.maxHeight = null;
+    image.src = "./assets/images/icon-plus.svg"
+  } else {
+    elementToHide.style.maxHeight = elementToHide.scrollHeight + "px";
+    image.src = "./assets/images/icon-minus.svg"
+  }
+}
 
 [1, 2, 3, 4].forEach(i => {
-document.querySelector(`#id${i}`).addEventListener("click", listener    );
+  document.querySelector(`#id${i}_btn`).addEventListener("click", listener);
 })
 
