11421441e5c70bb1f41742716ff1d9be42fcc6af
David Johansson <david.johansson4@tre.se>
2024-02-17 18:29:59 +0100

    Steff

--- public/2-faq-accordion/scripts/scripts.js
+++ public/2-faq-accordion/scripts/scripts.js
@@ -1,11 +1,32 @@
+
+const listener2 = (e) => {
+ 
+  const idOfClicked = e.target.id
+  const identifier = idOfClicked.substr(0, idOfClicked.indexOf('_'));
+  const idOfHidden = `${identifier}Over`;
+  const image = document.getElementById(`${identifier}_img`)
+
+  if(elementToHide.style.maxHeight){
+    elementToHide.style.maxHeight = null
+  }
+
+
+}
+ 
 const listener = (e) => {
   const idOfClicked = e.target.id
   const identifier = idOfClicked.substr(0, idOfClicked.indexOf('_'));
   const idOfHidden = `${identifier}Over`;
   const image = document.getElementById(`${identifier}_img`)
 
+
+  const appa = `${idOfClicked}_dettärrimligt_${identifier}_dettaärmindrerimligt_${image}`
+
+  const aba = "det verkar fungera med surround (trasiga lampor {levererar} 'eea'"
+
+
   // Get the reference to the element you want to hide
-  const elementToHide = document.getElementById(idOfHidden);
+  const elementToHide = document.getElementById;
 
   if (elementToHide.style.maxHeight) {
     elementToHide.style.maxHeight = null;
